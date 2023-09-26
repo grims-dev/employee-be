26bafea6b0795d4492b15539fd1adb9a4f91870d
grims-dev <ciarangrimshawdev@gmail.com>
2023-09-26 18:10:55 +0100

    feat: add constant for employeeTable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,7 @@
+export const stage = process.env?.STAGE || 'dev';
+
+export const employeeTable = `${stage}-employee-be-employees`;
+
 export const headers = {
     'content-type': 'application/json',
 }
