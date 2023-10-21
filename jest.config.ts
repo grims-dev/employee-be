2d20642b1d58374625028a4af08b85f473049da8
grims-dev <ciarangrimshawdev@gmail.com>
2023-10-21 16:03:50 +0100

    test: ignore certain paths

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
 	preset: 'ts-jest',
 	collectCoverageFrom: ['src/**/*.ts'],
+	testPathIgnorePatterns: ['node_modules', 'build'],
 };
 
 export default config;
